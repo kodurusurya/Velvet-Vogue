30f45e4edcfb05164f2c359018d96749040ff369
Chaitanya <bodayyagari.2@wright.edu>
2025-03-23 23:29:52 -0400

    Update db.js

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,1 +1,20 @@
+// config/db.js
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/VelvetVogue', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error('MongoDB connection error:', err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
